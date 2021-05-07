6aa47d5e19b1fc7e63ace8fb2266d0f90bdae4ee
RyoKitatani <ryo0929m@gmail.com>
2021-05-08 01:05:29 +0900

    Initial Commit

---
+++ RectangleApp/RectangleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAppApp.swift
+//  RectangleApp
+//
+//  Created by RyoKitatani on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
